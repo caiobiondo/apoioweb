0c5bdec6bd48ea46ea540c6e84787d294ae6e486
Fellipe Chagas <chagas.0@gmail.com>
2017-10-27 20:13:13 -0200

    [Feat] Uses theme provider and natura-ui cards

--- src/App.js
+++ src/App.js
@@ -5,20 +5,24 @@ import { IntlProvider } from 'react-intl';
 import Orders from './components/ecosystems/Orders';
 import OrderDetails from './components/ecosystems/OrderDetails';
 
-class App extends Component {
+import { ThemeProvider, theme, setupGlobals } from 'natura-ui';
+
+setupGlobals();
+
+export default class App extends Component {
   render() {
     return (
-      <IntlProvider>
-        <BrowserRouter>
-          <div>
-            <Route exact path="/my-orders" component={Orders} />
-            <Route path="/my-orders/:id" component={OrderDetails} />
-            <Redirect from="/" to="/my-orders" />
-          </div>
-        </BrowserRouter>
-      </IntlProvider>
+      <ThemeProvider theme={theme}>
+        <IntlProvider>
+          <BrowserRouter>
+            <div>
+              <Route exact path="/my-orders" component={Orders} />
+              <Route path="/my-orders/:id" component={OrderDetails} />
+              <Redirect from="/" to="/my-orders" />
+            </div>
+          </BrowserRouter>
+        </IntlProvider>
+      </ThemeProvider>
     );
   }
 }
-
-export default App;
--- src/components/ecosystems/Orders.js
+++ src/components/ecosystems/Orders.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { FormButton } from 'natura-ui';
+import { FormButton, Card } from 'natura-ui';
 
 class Orders extends Component {
   render() {
@@ -8,15 +8,15 @@ class Orders extends Component {
       <div>
         <h2>Meus Pedidos</h2>
         <ul>
-          <li key={1}>
-            <FormButton label="Pedido #1" link="my-orders/1" />
-          </li>
-          <li key={2}>
+          <Card key={1}>
+            <FormButton label="Pedido #1" primary={true} link="my-orders/1" />
+          </Card>
+          <Card key={2}>
             <Link to="my-orders/2">Pedido #2</Link>
-          </li>
-          <li key={3}>
+          </Card>
+          <Card key={3}>
             <Link to="my-orders/3">Pedido #3</Link>
-          </li>
+          </Card>
         </ul>
       </div>
     );
