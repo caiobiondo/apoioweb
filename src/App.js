a4878e0e9b1ac83b4e517c7d2ef0a7d957c4d946
Fellipe Chagas <chagas.0@gmail.com>
2017-10-30 13:58:22 -0200

    [Fix] Resolve warnings related to intl and router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
 
 import Orders from './components/ecosystems/Orders';
@@ -13,12 +13,11 @@ export default class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <IntlProvider>
+        <IntlProvider locale="pt-BR">
           <BrowserRouter>
             <div>
               <Route exact path="/my-orders" component={Orders} />
               <Route path="/my-orders/:id" component={OrderDetails} />
-              <Redirect from="/" to="/my-orders" />
             </div>
           </BrowserRouter>
         </IntlProvider>
