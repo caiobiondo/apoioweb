913bc603bddd211ec715cbc4fa88ff9545ae30df
Fellipe Chagas <chagas.0@gmail.com>
2017-10-30 20:32:27 -0200

    [Feat] Connects apollo and start using intl

--- src/components/ecosystems/Orders/Orders.js
+++ src/components/ecosystems/Orders/Orders.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import { FormButton, Card } from 'natura-ui';
 import { List } from './Orders.styles';
 import withData from './Orders.data';
+import { FormattedMessage } from 'react-intl';
 
 class Orders extends Component {
   render() {
     return (
       <div>
-        <h2>Meus Pedidos</h2>
+        <h2>
+          <FormattedMessage id="my_orders" />
+        </h2>
         <List>
           <Card key={1}>
             <FormButton label="Pedido #1" primary={true} link="my-orders/1" />
--- src/components/environment/App.js
+++ src/components/environment/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { IntlProvider } from 'react-intl';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClientCreator from '../../infra/ApolloClientCreator';
+import { GRAPHQL_URI } from '../../config';
 
 import { Main } from './App.styles';
 import Orders from '../ecosystems/Orders/Orders';
@@ -8,19 +11,25 @@ import OrderDetails from '../ecosystems/OrderDetails/OrderDetails';
 
 import { ThemeProvider, theme, setupGlobals } from 'natura-ui';
 
+import { locale, flattenMessages, messages } from '../../locale/index';
+
 setupGlobals();
+
+const client = new ApolloClientCreator(GRAPHQL_URI).create();
 
 export default class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <IntlProvider locale="pt-BR">
-          <BrowserRouter>
-            <Main>
-              <Route exact path="/my-orders" component={Orders} />
-              <Route path="/my-orders/:id" component={OrderDetails} />
-            </Main>
-          </BrowserRouter>
+        <IntlProvider locale={locale} messages={flattenMessages(messages[locale])}>
+          <ApolloProvider client={client}>
+            <BrowserRouter>
+              <Main>
+                <Route exact path="/my-orders" component={Orders} />
+                <Route path="/my-orders/:id" component={OrderDetails} />
+              </Main>
+            </BrowserRouter>
+          </ApolloProvider>
         </IntlProvider>
       </ThemeProvider>
     );
