4f959637194ad76795c558e28d5a0f581be6877e
Fellipe Chagas <chagas.0@gmail.com>
2017-10-31 17:23:00 -0200

    feat(web-layout) Use order component on orders list

--- src/components/ecosystems/Orders/Orders.data.js
+++ src/components/ecosystems/Orders/Orders.data.js
@@ -4,8 +4,12 @@ import gql from 'graphql-tag';
 const OrdersQuery = gql`
   query OrdersQuery {
     orders(id: 1) {
+      cycle
+      date
+      estimatedDeliveryDate
       orderNumber
       orderValue
+      status
       totalScore
     }
   }
--- src/components/ecosystems/Orders/Orders.js
+++ src/components/ecosystems/Orders/Orders.js
@@ -1,28 +1,51 @@
 import React, { Component } from 'react';
-import { FormButton, Card } from 'natura-ui';
+import { Card } from 'natura-ui';
 import { List } from './Orders.styles';
 import withData from './Orders.data';
-import { FormattedMessage } from 'react-intl';
+import Order from '../../molecules/Order/Order';
+import CustomCard from '../../molecules/CustomCard/CustomCard';
+
+const renderOrder = order => {
+  return (
+    <Order
+      key={order.orderNumber}
+      color={CustomCard.SUCCESS}
+      left={{
+        header: {
+          orderNumber: `#${order.orderNumber}`
+        },
+        body: {
+          orderDate: order.date,
+          orderCycle: order.cycle,
+          orderEstimatedDeliveryDate: order.estimatedDeliveryDate
+        }
+      }}
+      middle={{
+        header: {
+          orderValue: `R$ ${order.orderValue}`
+        },
+        body: {
+          orderTotalScore: order.totalScore
+        }
+      }}
+      right={{
+        status: order.status,
+        details: `my-orders/${order.orderNumber}`
+      }}
+    />
+  );
+};
 
 class Orders extends Component {
   render() {
     return (
-      <div>
-        <h2>
-          <FormattedMessage id="my_orders" />
-        </h2>
+      <Card>
         <List>
-          {(this.props.data.orders || []).map((order, index) => {
-            const label = `Pedido #${order.orderNumber}`;
-            const link = `my-orders/${order.orderNumber}`;
-            return (
-              <Card key={index}>
-                <FormButton label={label} primary={true} link={link} />
-              </Card>
-            );
+          {(this.props.data.orders || []).map(order => {
+            return renderOrder(order);
           })}
         </List>
-      </div>
+      </Card>
     );
   }
 }
