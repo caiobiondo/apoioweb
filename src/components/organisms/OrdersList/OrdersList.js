b31b087b44d09c868c1c124a5a472bbdc1b743d4
Fellipe Chagas <chagas.0@gmail.com>
2017-11-09 13:29:50 -0200

    feat(empyt-orders): Add loading and empty list handlers

--- src/components/organisms/OrdersList/OrdersList.js
+++ src/components/organisms/OrdersList/OrdersList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import { Loading } from 'natura-ui';
 import { List } from './OrdersList.styles';
 import OrdersListQuery from './OrdersList.data';
 import Order from 'components/molecules/Order/Order';
 import CustomCard from 'components/molecules/CustomCard/CustomCard';
+import EmptyOrders from 'components/molecules/EmptyOrders/EmptyOrders';
 import { graphql } from 'react-apollo';
 import { injectIntl } from 'react-intl';
 import { formatDate, formatCurrency } from 'locale/utils';
@@ -48,7 +50,13 @@ const renderOrder = (order, intl) => {
 export class OrdersList extends Component {
   render() {
     const { data, intl } = this.props;
-    return <List>{(data.orders || []).map(order => renderOrder(order, intl))}</List>;
+    if (data.loading) {
+      return <Loading />;
+    }
+    if (data.orders && data.orders.length === 0) {
+      return <EmptyOrders />;
+    }
+    return <List>{data.orders.map(order => renderOrder(order, intl))}</List>;
   }
 }
 
