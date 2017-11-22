526e983b5420b3468b7d950be70ab9812f3451d8
Vitor Margis <vitor.margis@gmail.com>
2017-11-22 01:03:36 -0200

    feature(order-details-refactor): Replaces old titlewith new title section component

--- src/components/organisms/OrderDetailsData/molecules/SectionTitle.js
+++ src/components/organisms/OrderDetailsData/molecules/SectionTitle.js
@@ -2,14 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Icon } from 'natura-ui';
 import { SectionTitleWrapper } from './SectionTitle.styles';
+import { FormattedMessage } from 'react-intl';
 
 const SectionTitle = props => {
-  const { iconName, children } = props;
+  const { iconName, children, value } = props;
 
   return (
     <SectionTitleWrapper {...props}>
       <Icon file={iconName} />
-      {children}
+      {value ? <FormattedMessage id={value} /> : children}
     </SectionTitleWrapper>
   );
 };
