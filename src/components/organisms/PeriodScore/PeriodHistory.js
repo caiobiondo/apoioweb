b04636ee7f0a55b7a1f0056722c5d2877ed65f98
Vitor Margis <vitor.margis@gmail.com>
2017-11-24 01:43:43 -0200

    feature(my-score-table): Adds intl for currency

--- src/components/organisms/PeriodScore/PeriodHistory.js
+++ src/components/organisms/PeriodScore/PeriodHistory.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Paper, Icon } from 'natura-ui';
+import { formatCurrency } from 'locale/utils';
+
 import {
   Wrapper,
   TableStyle,
@@ -23,146 +25,150 @@ import {
   TableFooter,
 } from 'material-ui/Table';
 
-const points = {
-  digital: 10,
-  presencial: 10,
-  totalCicles: 10,
-};
+const PeriodHistory = props => {
+  const { intl } = props;
 
-const tableData = {
-  columns: ['icon', 'order', 'date', 'points', 'value'],
-  style: {
-    icon: { width: '14%' },
-    order: { width: '30%' },
-    date: { width: '24%' },
-    points: { width: '15%' },
-    value: { width: '15%', textAlign: 'right' },
-  },
-  renderer: {
-    icon: iconPath => (
-      <IconWrapper>
-        <Icon file={iconPath} />
-      </IconWrapper>
-    ),
-    order: value => `Pedido ${value}`,
-    date: value => `Data ${value}`,
-    points: value => `${value}pts`,
-    value: value => `R$ ${value}`,
-  },
-  body: [
-    {
-      icon: 'ico_info',
-      order: '#361352429',
-      date: '24/02/2016',
-      points: '10',
-      value: '37.34',
+  const points = {
+    digital: 10,
+    presencial: 10,
+    totalCicles: 10,
+  };
+
+  const tableData = {
+    columns: ['icon', 'order', 'date', 'points', 'value'],
+    style: {
+      icon: { width: '14%' },
+      order: { width: '30%' },
+      date: { width: '24%' },
+      points: { width: '15%' },
+      value: { width: '15%', textAlign: 'right' },
     },
-    {
-      icon: 'ico_info',
-      order: '#361352429',
-      date: '24/02/2016',
-      points: '10',
-      value: '37.34',
+    renderer: {
+      icon: iconPath => (
+        <IconWrapper>
+          <Icon file={iconPath} />
+        </IconWrapper>
+      ),
+      order: value => `Pedido ${value}`,
+      date: value => `Data ${value}`,
+      points: value => `${value}pts`,
+      value: value => formatCurrency(value, intl),
     },
-    {
-      icon: 'ico_info',
-      order: '#361352429',
-      date: '24/02/2016',
-      points: '10',
-      value: '37.34',
-    },
-    {
-      icon: 'ico_info',
-      order: '#361352429',
-      date: '24/02/2016',
-      points: '10',
-      value: '37.34',
-    },
-  ],
-};
+    body: [
+      {
+        icon: 'ico_info',
+        order: '#361352429',
+        date: '24/02/2016',
+        points: '10',
+        value: '37.34',
+      },
+      {
+        icon: 'ico_info',
+        order: '#361352429',
+        date: '24/02/2016',
+        points: '10',
+        value: '37.34',
+      },
+      {
+        icon: 'ico_info',
+        order: '#361352429',
+        date: '24/02/2016',
+        points: '10',
+        value: '37.34',
+      },
+      {
+        icon: 'ico_info',
+        order: '#361352429',
+        date: '24/02/2016',
+        points: '10',
+        value: '37.34',
+      },
+    ],
+  };
 
-const renderCell = (value, key) => {
-  let cellStyle = TableCellStyle;
+  const renderCell = (value, key) => {
+    let cellStyle = TableCellStyle;
 
-  if (tableData.renderer && !tableData.renderer[key])
-    return <TableRowColumn key={key}>{value}</TableRowColumn>;
+    if (tableData.renderer && !tableData.renderer[key])
+      return <TableRowColumn key={key}>{value}</TableRowColumn>;
 
-  if (tableData.style && tableData.style[key])
-    cellStyle = { ...TableCellStyle, ...tableData.style[key] };
+    if (tableData.style && tableData.style[key])
+      cellStyle = { ...TableCellStyle, ...tableData.style[key] };
 
-  return (
-    <TableRowColumn key={key} style={cellStyle}>
-      {tableData.renderer[key](value)}
-    </TableRowColumn>
-  );
-};
+    return (
+      <TableRowColumn key={key} style={cellStyle}>
+        {tableData.renderer[key](value)}
+      </TableRowColumn>
+    );
+  };
 
-const renderTableHeader = () => {
-  if (!tableData.header) return null;
-  return (
-    <TableHeader adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
-      <TableRow style={{ border: 'none' }}>
-        {tableData.columns.map(cell => {
-          return <TableHeaderColumn key={cell}>{tableData.header[cell]}</TableHeaderColumn>;
-        })}
-      </TableRow>
-    </TableHeader>
-  );
-};
+  const renderTableHeader = () => {
+    if (!tableData.header) return null;
+    return (
+      <TableHeader adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
+        <TableRow style={{ border: 'none' }}>
+          {tableData.columns.map(cell => {
+            return <TableHeaderColumn key={cell}>{tableData.header[cell]}</TableHeaderColumn>;
+          })}
+        </TableRow>
+      </TableHeader>
+    );
+  };
 
-const renderTableBody = () => {
-  if (!tableData.body) return null;
-  return (
-    <TableBody displayRowCheckbox={false}>
-      {tableData.body.map((row, index) => {
-        return (
-          <TableRow key={index} style={TableRowStyle}>
-            {tableData.columns.map(cell => {
-              return renderCell(row[cell], cell);
-            })}
-          </TableRow>
-        );
-      })}
-    </TableBody>
-  );
-};
-
-const renderTableFooter = () => {
-  if (!tableData.footer) return null;
-  return (
-    <TableFooter adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
-      <TableRow style={{ border: 'none' }}>
-        {tableData.columns.map(cell => {
-          return <TableRowColumn key={cell}>{tableData.header[cell]}</TableRowColumn>;
-        })}
-      </TableRow>
-    </TableFooter>
-  );
-};
-
-const PeriodHistory = () => (
-  <Paper zDepth={2} style={Wrapper}>
-    <OrderHistoryHeader>
-      <OrderHistoryTitle>
-        <FormattedMessage id="orderHistory" />
-      </OrderHistoryTitle>
-      <OrderHistoryPoints>
-        {Object.keys(points).map((key, index) => {
+  const renderTableBody = () => {
+    if (!tableData.body) return null;
+    return (
+      <TableBody displayRowCheckbox={false}>
+        {tableData.body.map((row, index) => {
           return (
-            <OrderHistoryPoint key={key}>
-              {key}: {points[key]}pts
-            </OrderHistoryPoint>
+            <TableRow key={index} style={TableRowStyle}>
+              {tableData.columns.map(cell => {
+                return renderCell(row[cell], cell);
+              })}
+            </TableRow>
           );
         })}
-      </OrderHistoryPoints>
-    </OrderHistoryHeader>
-    <Table style={TableStyle}>
-      {renderTableHeader()}
-      {renderTableBody()}
-      {renderTableFooter()}
-    </Table>
-  </Paper>
-);
+      </TableBody>
+    );
+  };
+
+  const renderTableFooter = () => {
+    if (!tableData.footer) return null;
+    return (
+      <TableFooter adjustForCheckbox={false} displaySelectAll={false} enableSelectAll={false}>
+        <TableRow style={{ border: 'none' }}>
+          {tableData.columns.map(cell => {
+            return <TableRowColumn key={cell}>{tableData.header[cell]}</TableRowColumn>;
+          })}
+        </TableRow>
+      </TableFooter>
+    );
+  };
+
+  return (
+    <Paper zDepth={2} style={Wrapper}>
+      <OrderHistoryHeader>
+        <OrderHistoryTitle>
+          <FormattedMessage id="orderHistory" />
+        </OrderHistoryTitle>
+        <OrderHistoryPoints>
+          {Object.keys(points).map((key, index) => {
+            return (
+              <OrderHistoryPoint key={key}>
+                {key}: {points[key]}pts
+              </OrderHistoryPoint>
+            );
+          })}
+        </OrderHistoryPoints>
+      </OrderHistoryHeader>
+      <Table style={TableStyle}>
+        {renderTableHeader()}
+        {renderTableBody()}
+        {renderTableFooter()}
+      </Table>
+    </Paper>
+  );
+};
 
 const PeriodHistoryIntl = injectIntl(PeriodHistory);
 export default PeriodHistoryIntl;
