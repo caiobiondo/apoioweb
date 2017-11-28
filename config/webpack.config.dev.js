f4154f19263df6539ddd64d45d4c06a26e4e5fc0
Thiago <thiago.porto@vizir.com.br>
2017-11-28 19:39:31 -0200

    feat(webpack): Added plugin to avoid duplicating the file name in the imports
    With this plugin `import PeriodScore from components/organisms/PeriodScore/PeriodScore;` becomes `import PeriodScore from components/organisms/PeriodScore;`

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -13,6 +13,7 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const getClientEnvironment = require('./env');
 const paths = require('./paths');
 const convertDimensions = require('./webpack/svgo/convertDimensions');
+const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // In development, we always serve from the root. This makes config easier.
@@ -100,6 +101,10 @@ module.exports = {
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+      new DirectoryNamedWebpackPlugin({
+        honorIndex: true,
+        exclude: /node_modules/
+      }),
     ],
   },
   module: {
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -14,6 +14,7 @@ const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
 const convertDimensions = require('./webpack/svgo/convertDimensions');
+const DirectoryNamedWebpackPlugin = require("directory-named-webpack-plugin");
 
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
@@ -104,6 +105,10 @@ module.exports = {
       // please link the files into your node_modules/ and let module-resolution kick in.
       // Make sure your source files are compiled, as they will not be processed in any way.
       new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson]),
+      new DirectoryNamedWebpackPlugin({
+        honorIndex: true,
+        exclude: /node_modules/
+      }),
     ],
   },
   module: {
