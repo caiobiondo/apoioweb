bdddddf95d745be3d3b78472ce94832d1009f8d1
Vitor Margis <vitor.margis@gmail.com>
2017-11-30 19:59:31 -0200

    feature(customer-list): Adds baisc deletion simulation

--- src/components/atoms/CustomerButton/CustomerButton.js
+++ src/components/atoms/CustomerButton/CustomerButton.js
@@ -4,7 +4,7 @@ import { IconWrapper, Wrapper } from './CustomerButton.styles';
 
 const OrderAddButton = props => {
   return (
-    <Wrapper remove={props.remove}>
+    <Wrapper onClick={() => props.action()} remove={props.remove}>
       <FloatingActionButton iconWrapper={IconWrapper}>
         <Icon file="ico_times" />
       </FloatingActionButton>
--- src/components/ecosystems/Customers/List/index.js
+++ src/components/ecosystems/Customers/List/index.js
@@ -7,11 +7,13 @@ class CustomersListWrapper extends Component {
   constructor(props) {
     super(props);
     this.selectCustomer = this.selectCustomer.bind(this);
+    this.removeCustomer = this.removeCustomer.bind(this);
   }
 
   state = {
     empty: false,
-    customerSelected: false,
+    isCustomerSelected: false,
+    customersSelected: [],
   };
 
   onLoadFinished = (empty, loading) => {
@@ -19,15 +21,20 @@ class CustomersListWrapper extends Component {
   };
 
   selectCustomer(customers) {
-    this.setState({ customerSelected: customers.length });
+    this.setState({ isCustomerSelected: customers.length, customersSelected: customers });
+  }
+
+  removeCustomer() {
+    if (!this.state.customersSelected.length) return;
+    console.log(this.state.customersSelected);
   }
 
   render() {
-    const { loading, customerSelected } = this.state;
+    const { loading, isCustomerSelected } = this.state;
     return (
       <Main loading={loading}>
-        <OrderAddButtonContainer remove={customerSelected}>
-          <CustomerButton remove={customerSelected} />
+        <OrderAddButtonContainer remove={isCustomerSelected}>
+          <CustomerButton action={this.removeCustomer} remove={isCustomerSelected} />
         </OrderAddButtonContainer>
         <CustomersList select={this.selectCustomer} />
       </Main>
