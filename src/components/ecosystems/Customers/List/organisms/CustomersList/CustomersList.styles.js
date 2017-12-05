fca6592f2ea5d937b83f9d8c290f3f43aa5c0dfd
Vitor Margis <vitor.margis@gmail.com>
2017-12-05 18:44:25 -0200

    feature(customer-list): Adds responsive styling for table

--- src/components/ecosystems/Customers/List/organisms/CustomersList/CustomersList.styles.js
+++ src/components/ecosystems/Customers/List/organisms/CustomersList/CustomersList.styles.js
@@ -7,7 +7,6 @@ export const Wrapper = {
   flexDirection: 'column',
   justifyContent: 'center',
   overflowY: 'scroll',
-  margin: '24px 70px 24px 24px',
 };
 
 export const cellStyle = {
@@ -37,6 +36,29 @@ export const TableWrapper = styled.div`
     }
 
     background: #f6f6f6;
+  }
+
+  @media (max-width: ${Responsive.VIEWPORT.MEDIUM}px) {
+    tr td,
+    tr th {
+      display: none;
+    }
+
+    tr th:first-child,
+    tr td:first-child {
+      display: table-cell;
+      width: 1px !important;
+    }
+
+    tr th:nth-child(2),
+    tr td:nth-child(2) {
+      display: table-cell;
+    }
+
+    td:first-child,
+    td:first-child * {
+      opacity: 1 !important;
+    }
   }
 `;
 
