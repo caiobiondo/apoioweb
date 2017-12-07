aeab6d52c1b248518b0a583195908e91ecf81656
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2017-12-07 18:57:46 -0200

    fix(customer-profile-cnotoken-tests): Fixing tests that broke after adding cnotoken handling

--- src/infra/ApolloClientCreator.test.js
+++ src/infra/ApolloClientCreator.test.js
@@ -1,20 +1,24 @@
 import ApolloClientCreator from './ApolloClientCreator';
+import { ACCESS_TOKEN_LOCAL_STORAGE_KEY, CNO_TOKEN_LOCAL_STORAGE_KEY } from 'config';
 
 describe('ApolloClientCreator', () => {
   test('creates an Apollo Client', () => {
     // given
     const uri = 'a graphql-uri';
     const sso = '{ "accessToken": "an-access-token", "clientId": "a-client-id" }';
-    const accessTokenKey = 'a access-token key';
+    const cnoToken = 'a-cno-token';
     const expectedClient = { client: 'an apollo client' };
     const expectedCache = { cache: 'a cache' };
     const expectedLink = 'a link';
     const apolloConstructorMock = jest.fn();
     const inMemoryCacheMock = jest.fn();
     const createHttpLink = jest.fn().mockReturnValue(expectedLink);
-    const localStorageGetItemMock = jest.fn().mockReturnValue(sso);
+    const localStorageMock = key => {
+      if (key === ACCESS_TOKEN_LOCAL_STORAGE_KEY) return sso;
+      if (key === CNO_TOKEN_LOCAL_STORAGE_KEY) return cnoToken;
+    };
 
-    window.localStorage.getItem = localStorageGetItemMock;
+    window.localStorage.getItem = localStorageMock;
 
     class ApolloClient {
       constructor(props) {
@@ -32,7 +36,8 @@ describe('ApolloClientCreator', () => {
     // when
     const creator = new ApolloClientCreator(
       uri,
-      accessTokenKey,
+      ACCESS_TOKEN_LOCAL_STORAGE_KEY,
+      CNO_TOKEN_LOCAL_STORAGE_KEY,
       ApolloClient,
       InMemoryCache,
       createHttpLink,
@@ -46,6 +51,7 @@ describe('ApolloClientCreator', () => {
     expect(createHttpLink).toBeCalledWith({
       headers: {
         accessToken: 'an-access-token',
+        cnotoken: 'a-cno-token',
         clientId: 'a-client-id',
       },
       uri: uri,
