30cd8d7b3005bd18dbfd39217108c7a16d2e4610
Daniel Sakuma <dsakuma@gmail.com>
2017-12-11 16:22:21 -0200

    feat(redirect-login): Add specs

--- src/hocs/withAuthErrorHandler/withAuthErrorHandler.test.js
+++ src/hocs/withAuthErrorHandler/withAuthErrorHandler.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
 import WithAuthErrorHandler, {
   needRedirectToLogin,
+  redirectToLogin,
   API_AUTH_ERROR_MSG,
+  LOGIN_PATH,
 } from './withAuthErrorHandler';
 
 class WrappedComponent extends React.Component {
@@ -11,7 +13,16 @@ class WrappedComponent extends React.Component {
   }
 }
 
-describe('withUserData HOC', () => {
+class LocalStorageMock {
+  clear() {
+    this.store = {};
+  }
+}
+
+// global.window = { location: { host: 'example.com' } };
+global.localStorage = new LocalStorageMock();
+
+describe('needRedirectToLogin', () => {
   it('should return true when there is an api auth error message', () => {
     //given
     const data = { error: API_AUTH_ERROR_MSG };
@@ -23,6 +34,34 @@ describe('withUserData HOC', () => {
     expect(result).toBeTruthy();
   });
 
+  it('should return false when there is not an api auth error message', () => {
+    //given
+    const data = { error: 'another error' };
+
+    //when
+    const result = needRedirectToLogin(data);
+
+    //then
+    expect(result).toBeFalsy();
+  });
+});
+
+describe('redirectToLogin', () => {
+  it('should clear local storage and redirect to login', () => {
+    //given
+    const clearSpy = jest.spyOn(global.localStorage, 'clear');
+    window.location.assign = jest.fn();
+
+    //when
+    redirectToLogin();
+
+    //then
+    expect(clearSpy).toHaveBeenCalled();
+    expect(window.location.assign).toBeCalledWith(LOGIN_PATH);
+  });
+});
+
+describe('withAuthErrorHandler HOC', () => {
   it('should render the child component without auth api error', () => {
     const renderer = new ShallowRenderer();
     const data = {};
