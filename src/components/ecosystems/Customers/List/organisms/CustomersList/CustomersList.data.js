dfd3e568239b54332070cd3e2c6b2f66b16766c6
Vitor Margis <vitor.margis@gmail.com>
2017-12-12 13:57:46 -0200

    feature(customer-list): Fixes graphql queries

--- src/components/ecosystems/Customers/List/organisms/CustomersList/CustomersList.data.js
+++ src/components/ecosystems/Customers/List/organisms/CustomersList/CustomersList.data.js
@@ -1,16 +1,20 @@
 import gql from 'graphql-tag';
 
-const ITEMS_PER_PAGE = 10;
-
 export const CustomersListQuery = gql`
-  query CustomersListQuery($offset: Int!, $limit: Int!) {
-    customers(offset: $offset, limit: $limit) {
-      code
+  query CustomersListQuery {
+    customers {
+      id
+      nickname
       name
-      avatar
-      email
-      phone
-      operator
+      gender
+      comment
+      emails {
+        email
+      }
+      phones {
+        phone
+        provider
+      }
     }
   }
 `;
