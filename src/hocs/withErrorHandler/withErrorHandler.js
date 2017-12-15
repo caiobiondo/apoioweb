2701e1fa01d933f1788aa85f1510a97424e8b6ae
Thiago <thiago.porto@vizir.com.br>
2017-12-15 20:14:04 -0200

    feat(error-handler): Add error handler

---
+++ src/hocs/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react';
+import { Modal, ModalButton } from 'natura-ui';
+import { withRouter } from 'react-router-dom';
+
+export const WithErrorHandler = Component => {
+  return class WithErrorHandlerComponent extends PureComponent {
+    constructor(props) {
+      super(props);
+      this.handleClose = this.handleClose.bind(this);
+      this.renderComponent = this.renderComponent.bind(this);
+      this.state = {
+        open: false,
+        title: 'Erro',
+        body: 'Desculpe, houve um erro no processamento',
+        actions: [<ModalButton label="OK, entendi" primary={true} onClick={this.handleClose} />],
+        errored: false,
+      };
+    }
+
+    componentDidCatch(error, info) {
+      console.log(error, info);
+      this.setState({ open: true, errored: true });
+    }
+
+    handleClose(event) {
+      event.stopPropagation();
+      this.setState({ open: false }, () => {
+        this.props.history.goBack();
+      });
+    }
+
+    renderComponent() {
+      if (this.state.errored) return null;
+
+      return <Component {...this.props} />;
+    }
+
+    render() {
+      return (
+        <div>
+          <Modal
+            width={425}
+            maxWidth={'85%'}
+            open={this.state.open}
+            title={this.state.title}
+            actions={this.state.actions}
+          >
+            {this.state.body}
+          </Modal>
+          {this.renderComponent()}
+        </div>
+      );
+    }
+  };
+};
+
+export default Component => {
+  return withRouter(WithErrorHandler(Component));
+};
