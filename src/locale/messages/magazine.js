2e8e701096ce528af811647117609e38e04c7c2d
Fellipe Chagas <chagas.0@gmail.com>
2018-01-16 20:06:14 -0200

    feat(main-magazine): Finishes layout on current magazine

--- src/locale/messages/magazine.js
+++ src/locale/messages/magazine.js
@@ -4,7 +4,8 @@ export default {
     currentMagazine: 'Revista Atual',
     downloadMagazine: 'Baixar revista',
     magazineCycle: 'Ciclo',
-    taxInfoTitle: 'Confira aqui a carga tributária dos produtos Natura.',
+    taxViewMore: 'Confira aqui',
+    taxInfoTitle: 'a carga tributária dos produtos Natura.',
     taxInfoDescription:
       'Os valores deste material são apenas sugestões que oferecemos aos Consultores Natura, que são absolutamente livre para revender os produtos pelos valores que melhor lhes atendem',
     visualizeMagazine: 'Ver revista',
