35b8d951153c1accc7a3914fbf916279c108b6bc
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2018-02-23 14:56:55 -0300

    feat(training-courses-list): Refactoring some things

--- src/components/ecosystems/Training/Courses/List/organisms/TrainingCoursesList/TrainingCoursesList.js
+++ src/components/ecosystems/Training/Courses/List/organisms/TrainingCoursesList/TrainingCoursesList.js
@@ -3,9 +3,16 @@ import EmptyList from 'components/molecules/EmptyList/EmptyList';
 import TrainingCourse from 'components/ecosystems/Training/Courses/List/molecules/TrainingCourse';
 import { Loading, CircularProgress, Paper } from 'natura-ui';
 import { TrainingCoursesQuery, TrainingCoursesQueryOptions } from './TrainingCoursesList.data';
+import { TrainingCourseUpdateMutation } from 'components/ecosystems/Training/data/TrainingCourseUpdate.data';
 import PageMenu from 'components/ecosystems/Training/atoms/PageMenu/PageMenu';
-import { graphql } from 'react-apollo';
-import TrainingCourseMenuItems from 'components/ecosystems/Training/organisms/TrainingCourseMenuItems';
+import { graphql, compose } from 'react-apollo';
+
+import { translate } from 'locale';
+import MenuItem from 'material-ui/MenuItem';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import { RobotoRegular } from 'styles/typography';
 
 import InfiniteScroll from 'react-infinite-scroller';
 
@@ -17,13 +24,20 @@ import {
 } from './TrainingCoursesList.styles';
 
 export class TrainingCoursesList extends Component {
-  state = {
-    hasMoreItems: true,
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasMoreItems: true,
+      courses: [],
+    };
+  }
 
   componentWillReceiveProps({ loading, courses }) {
     this.notifyLoadFinish(loading, courses);
     this.checkIfHasMoreItems(loading, courses);
+
+    this.setState({ courses });
   }
 
   notifyLoadFinish = (loading, courses) => {
@@ -38,7 +52,7 @@ export class TrainingCoursesList extends Component {
     }
 
     const hasMoreItems =
-      (courses && !this.props.courses) || courses.length !== this.props.courses.length;
+      (courses && !this.state.courses) || courses.length !== this.state.courses.length;
     this.setState({ hasMoreItems });
   };
 
@@ -50,12 +64,103 @@ export class TrainingCoursesList extends Component {
     return !loading && (!courses || courses.length === 0);
   };
 
+  handleMenuItemClick = (event, child) => {
+    this.props
+      .mutate({
+        variables: {
+          input: { action: child.props.value },
+          sellerId: this.props.user.codigo,
+          courseId: child.props.course.id,
+        },
+      })
+      .then(response => {
+        if (response.error) {
+          // Handle error
+          return;
+        }
+
+        if (!response.data.updateCourse.status) {
+          // Handle not updated
+          return;
+        }
+
+        // Handle update success
+        const courses = this.state.courses.map(c => {
+          const course = { ...c };
+
+          if (course.id === child.props.course.id) {
+            if (course.isfavorite === 'true') {
+              course.isfavorite = 'false';
+            } else {
+              course.isfavorite = 'true';
+            }
+          }
+
+          return course;
+        });
+
+        this.setState({ courses });
+
+        return;
+      })
+      .catch(err => {
+        console.log('err', err);
+        // Handle error
+      });
+  };
+
+  renderMenuItems = course => {
+    const style = { fontFamily: RobotoRegular };
+
+    if (course.isfavorite === 'true') {
+      return (
+        <IconMenu
+          iconButtonElement={
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          }
+          anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+          targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+          onItemTouchTap={this.handleMenuItemClick}
+        >
+          <MenuItem
+            style={style}
+            primaryText={translate('trainingRemoveCourseMyList')}
+            value="unfavorite"
+            course={course}
+          />
+        </IconMenu>
+      );
+    }
+
+    return (
+      <IconMenu
+        iconButtonElement={
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        }
+        anchorOrigin={{ horizontal: 'right', vertical: 'top' }}
+        targetOrigin={{ horizontal: 'right', vertical: 'top' }}
+        onItemTouchTap={this.handleMenuItemClick}
+      >
+        <MenuItem
+          style={style}
+          primaryText={translate('trainingAddCourseMyList')}
+          value="favorite"
+          course={course}
+        />
+      </IconMenu>
+    );
+  };
+
   render() {
-    if (!this.props.courses && this.props.loading) {
+    if (!this.state.courses && this.props.loading) {
       return <Loading background="transparent" />;
     }
 
-    if (!this.props.courses || !this.props.courses.length) {
+    if (!this.state.courses || !this.state.courses.length) {
       return (
         <Paper style={fullContainer}>
           <PageMenu />
@@ -81,9 +186,9 @@ export class TrainingCoursesList extends Component {
           }
         >
           <List>
-            {this.props.courses.map((course, index) => (
+            {this.state.courses.map((course, index) => (
               <TrainingCourse key={index} course={course}>
-                <TrainingCourseMenuItems course={course} user={this.props.user} />
+                {this.renderMenuItems(course)}
               </TrainingCourse>
             ))}
           </List>
@@ -93,4 +198,7 @@ export class TrainingCoursesList extends Component {
   }
 }
 
-export default graphql(TrainingCoursesQuery, TrainingCoursesQueryOptions)(TrainingCoursesList);
+export default compose(
+  graphql(TrainingCoursesQuery, TrainingCoursesQueryOptions),
+  graphql(TrainingCourseUpdateMutation),
+)(TrainingCoursesList);
