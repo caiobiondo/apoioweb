65fd6e21b67f60755cd1eb6dbbf895d9adb81142
Leonardo Cardoso <leonardo@vizir.com.br>
2018-03-07 13:52:02 -0300

    (chore): Remove a block of code that was not useful for TrainingCategoriesList

--- src/components/ecosystems/Training/Categories/List/organisms/TrainingCategoriesList/TrainingCategoriesList.data.test.js
+++ src/components/ecosystems/Training/Categories/List/organisms/TrainingCategoriesList/TrainingCategoriesList.data.test.js
@@ -1,7 +1,6 @@
 import {
   TrainingCategoriesQuery,
   TrainingCategoriesQueryOptions,
-  updateQuery,
 } from './TrainingCategoriesList.data';
 
 describe('TrainingCategoriesQuery', () => {
@@ -25,47 +24,15 @@ describe('TrainingCategoriesQuery', () => {
   });
 
   it('should be the correct query props', () => {
-    const fetchMore = jest.fn().mockReturnValue('application of fetchMore');
     const data = {
       data: {
         loading: true,
         trainingCategories: [],
-        fetchMore: fetchMore,
       },
     };
 
     const props = TrainingCategoriesQueryOptions.props(data);
-    const fetchMoreResult = props.fetchMore();
 
     expect(props).toMatchSnapshot();
-    expect(fetchMore).toBeCalledWith({ variables: { offset: 0 }, updateQuery });
-    expect(fetchMoreResult).toEqual('application of fetchMore');
-  });
-
-  describe('updateQuery', () => {
-    it('should return the previous result', () => {
-      const previousResult = {
-        trainingCategories: [1, 2],
-      };
-
-      const result = updateQuery(previousResult, { fetchMoreResult: null });
-
-      expect(result).toEqual(previousResult);
-    });
-
-    it('should return the merge from previous and fetchMore result', () => {
-      const previousResult = {
-        trainingCategoriesWithCourses: [1, 2],
-      };
-      const trainingCategoriesWithCourses = [3, 4];
-
-      const result = updateQuery(previousResult, {
-        fetchMoreResult: { trainingCategoriesWithCourses },
-      });
-
-      expect(result).toEqual({
-        trainingCategoriesWithCourses: [1, 2, 3, 4],
-      });
-    });
   });
 });
