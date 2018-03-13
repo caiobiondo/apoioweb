de236b0bbff47d0717560268a53c9d21718be304
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-03-13 15:24:39 -0300

    (feature/training-course-view): Mutate video course status

--- src/components/ecosystems/Training/Courses/View/molecules/CourseContent/CourseContent.js
+++ src/components/ecosystems/Training/Courses/View/molecules/CourseContent/CourseContent.js
@@ -4,20 +4,80 @@ import { ContentWrapper } from './CourseContent.styles';
 import { Player, BigPlayButton } from 'video-react';
 import CourseEvaluation from '../CourseEvaluation';
 import 'video-react/dist/video-react.css';
+import { TrainingCourseUpdateMutation } from 'components/ecosystems/Training/data/TrainingCourseUpdate.data';
+import { graphql } from 'react-apollo';
 
 export class CourseContent extends Component {
   state = {
     ended: false,
+    hasStarted: false,
+    paused: false,
+    hasNotSentInitializeStatus: true,
+    hasNotSentPausedStatus: true,
+    hasNotSentEndedStatus: true,
+    currentTime: 0,
   };
 
   componentDidMount() {
     this.refs.player.subscribeToStateChange(this.handleStateChange.bind(this));
   }
 
+  defineVideoCourseStatus = () => {
+    if (this.state.hasNotSentInitializeStatus && this.state.hasStarted) {
+      this.mutateVideoCourseStatus('initialized');
+      this.setState({ hasNotSentInitializeStatus: false });
+    }
+
+    if (
+      !this.state.hasNotSentInitializeStatus &&
+      this.state.hasNotSentPausedStatus &&
+      this.state.paused
+    ) {
+      this.mutateVideoCourseStatus('paused');
+      this.setState({ hasNotSentPausedStatus: false });
+    }
+
+    if (this.state.hasNotSentEndedStatus && this.state.ended) {
+      this.mutateVideoCourseStatus('terminated');
+      this.setState({ hasNotSentEndedStatus: false });
+    }
+  };
+
+  roundCurrentTime = () => Math.round(this.state.currentTime).toString();
+
+  mutateVideoCourseStatus = action => {
+    let input = { action };
+
+    if (this.state.paused) {
+      input = { ...input, stoppedAt: this.roundCurrentTime() };
+    }
+
+    if (this.state.ended) {
+      input = { ...input, stoppedAt: '1' };
+    }
+
+    this.props
+      .mutate({
+        variables: {
+          input,
+          sellerId: this.props.sellerId,
+          courseId: this.props.course.id,
+        },
+      })
+      .catch(err => {
+        console.log('err', err);
+      });
+  };
+
   handleStateChange(state, prevState) {
     this.setState({
       ended: state.ended,
+      hasStarted: state.hasStarted,
+      paused: state.paused,
+      currentTime: state.currentTime,
     });
+
+    this.defineVideoCourseStatus();
   }
 
   canRenderEvaluation = () => this.props.course.ratedByYou !== 'true' && this.state.ended;
@@ -48,4 +108,4 @@ CourseContent.propTypes = {
   sellerId: PropTypes.number.isRequired,
 };
 
-export default CourseContent;
+export default graphql(TrainingCourseUpdateMutation)(CourseContent);
--- src/components/ecosystems/Training/Courses/View/molecules/CourseViewHeader/CourseViewHeader.js
+++ src/components/ecosystems/Training/Courses/View/molecules/CourseViewHeader/CourseViewHeader.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { Row, Col } from 'react-flexbox-grid';
 import { Icon } from 'natura-ui';
 import { Link } from 'react-router-dom';
