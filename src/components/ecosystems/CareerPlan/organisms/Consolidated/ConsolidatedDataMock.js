686def313e78f38d0ddc7f8adb7a2514f713bde1
Leonardo Cardoso <leonardocardosodealmeida@gmail.com.br>
2018-03-14 19:05:00 -0300

    feat(career-plan-indicator): Control which cycles to show by range inside consolidate area

--- src/components/ecosystems/CareerPlan/organisms/Consolidated/ConsolidatedDataMock.js
+++ src/components/ecosystems/CareerPlan/organisms/Consolidated/ConsolidatedDataMock.js
@@ -2,52 +2,98 @@ export default [
   {
     cycle: 1,
     overcoming: 1233,
-    value: 'string',
+    value: 'Supera',
   },
   {
     cycle: 2,
     overcoming: 5543,
-    value: 'string',
+    value: 'Abaixo',
   },
   {
     cycle: 3,
     overcoming: 543,
-    value: 'string',
+    value: 'Abaixo',
   },
   {
     cycle: 4,
     overcoming: 45,
-    value: 'string',
+    value: 'Atende',
   },
   {
     cycle: 5,
     overcoming: 0,
-    value: 'string',
+    value: 'Excede',
     current: true,
   },
   {
     cycle: 6,
     overcoming: 0,
-    value: 'string',
+    value: 'Supera',
   },
   {
     cycle: 7,
     overcoming: 0,
-    value: 'string',
+    value: 'Dentro',
   },
   {
     cycle: 8,
     overcoming: 0,
-    value: 'string',
+    value: 'Excede',
   },
   {
     cycle: 9,
     overcoming: 0,
-    value: 'string',
+    value: 'Supera',
   },
   {
     cycle: 10,
     overcoming: 0,
-    value: 'string',
+    value: 'Supera',
+  },
+  {
+    cycle: 11,
+    overcoming: 1233,
+    value: 'Supera',
+  },
+  {
+    cycle: 12,
+    overcoming: 5543,
+    value: 'Abaixo',
+  },
+  {
+    cycle: 13,
+    overcoming: 543,
+    value: 'Abaixo',
+  },
+  {
+    cycle: 14,
+    overcoming: 45,
+    value: 'Atende',
+  },
+  {
+    cycle: 15,
+    overcoming: 0,
+    value: 'Excede',
+    current: true,
+  },
+  {
+    cycle: 16,
+    overcoming: 0,
+    value: 'Supera',
+  },
+  {
+    cycle: 17,
+    overcoming: 0,
+    value: 'Dentro',
+  },
+  {
+    cycle: 18,
+    overcoming: 0,
+    value: 'Excede',
+  },
+  {
+    cycle: 19,
+    overcoming: 0,
+    value: 'Supera',
   },
 ];
