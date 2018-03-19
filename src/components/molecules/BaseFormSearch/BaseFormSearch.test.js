1fab02ffce7b2e8b3f7b5e1dda2d0b0644046816
Lucas Lima de Souza <lucas.lima@vizir.com.br>
2018-03-19 14:10:30 -0300

    (feature/training-search-course): Fixed BaseFormSearch tests

--- src/components/molecules/BaseFormSearch/BaseFormSearch.test.js
+++ src/components/molecules/BaseFormSearch/BaseFormSearch.test.js
@@ -3,20 +3,25 @@ import { shallow } from 'enzyme';
 import { BaseFormSearch } from './BaseFormSearch';
 import toJson from 'enzyme-to-json';
 
+const baseFormSearchProps = {
+  onSearch: jest.fn(),
+  searchValue: '',
+  sectionTitle: { iconName: 'ico_add_customer', value: 'myCustomers' },
+  description: 'customersSearchInfo',
+  inputLabel: 'customerName',
+};
+
 describe('BaseFormSearch Organism', () => {
   it('should render input and submit button', () => {
-    const onSearch = jest.fn();
-
-    const result = shallow(<BaseFormSearch onSearch={onSearch} />);
+    const result = shallow(<BaseFormSearch {...baseFormSearchProps} />);
 
     expect(toJson(result)).toMatchSnapshot();
   });
 
   it("should update state's name with value", () => {
-    const onSearch = jest.fn();
     const expectedName = 'a name';
 
-    const result = shallow(<BaseFormSearch onSearch={onSearch} />);
+    const result = shallow(<BaseFormSearch {...baseFormSearchProps} />);
     const instance = result.instance();
     instance.handleNameChange(null, expectedName);
 
@@ -24,48 +29,47 @@ describe('BaseFormSearch Organism', () => {
   });
 
   it('should call onSearch with filters', () => {
-    const onSearch = jest.fn();
     const event = {
       stopPropagation: jest.fn(),
     };
     const name = 'a name';
 
-    const result = shallow(<BaseFormSearch onSearch={onSearch} />);
+    const result = shallow(<BaseFormSearch {...baseFormSearchProps} />);
     const instance = result.instance();
     instance.setState({ name });
     instance.onSubmit(event);
 
-    expect(onSearch).toBeCalledWith({ name });
+    expect(baseFormSearchProps.onSearch).toBeCalledWith({ name });
     expect(event.stopPropagation).toBeCalled();
   });
 
   it('should call onSearch with filters when enter is pressed', () => {
-    const onSearch = jest.fn();
     const event = {
       key: 'Enter',
     };
     const name = 'a name';
 
-    const result = shallow(<BaseFormSearch onSearch={onSearch} />);
+    const result = shallow(<BaseFormSearch {...baseFormSearchProps} />);
     const instance = result.instance();
     instance.setState({ name });
     instance.onKeyPress(event);
 
-    expect(onSearch).toBeCalledWith({ name });
+    expect(baseFormSearchProps.onSearch).toBeCalledWith({ name });
   });
 
   it('should not call onSearch with filters when enter is not pressed', () => {
     const onSearch = jest.fn();
+    const newBaseFormSearchProps = { ...baseFormSearchProps, onSearch };
     const event = {
       key: 'K',
     };
     const name = 'a name';
 
-    const result = shallow(<BaseFormSearch onSearch={onSearch} />);
+    const result = shallow(<BaseFormSearch {...newBaseFormSearchProps} />);
     const instance = result.instance();
     instance.setState({ name });
     instance.onKeyPress(event);
 
-    expect(onSearch).not.toBeCalled();
+    expect(newBaseFormSearchProps.onSearch).not.toBeCalled();
   });
 });
