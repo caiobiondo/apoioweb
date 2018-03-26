6bbcf066a8e75369532d97fd18e97134fb01e196
Leonardo Cardoso <leonardocardosodealmeida@gmail.com.br>
2018-03-26 19:06:03 -0300

    test(career-plan-components): Test IndicatorList component

---
+++ src/components/ecosystems/CareerPlan/Anual/organisms/IndicatorList/IndicatorList.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { IndicatorList } from './IndicatorList';
+
+const setup = propOverrides => {
+  const props = Object.assign(propOverrides);
+
+  const result = shallow(<IndicatorList {...props} />);
+
+  return {
+    props,
+    result,
+  };
+};
+
+describe('IndicatorList', () => {
+  describe('on getPastIndicator method', () => {
+    it('should get the past indicator, given an indicator type', () => {
+      // given
+      const props = {
+        indicators: [
+          {
+            indicatorType: 'scoresTotal',
+            cycles: [],
+          },
+        ],
+        pastIndicators: [
+          {
+            indicatorType: 'scoresTotal',
+            cycles: [],
+          },
+        ],
+      };
+
+      // when
+      const { result } = setup(props);
+      const instance = result.instance();
+      const pastIndicator = instance.getPastIndicator('scoresTotal');
+
+      // then
+      expect(pastIndicator).toEqual(props.pastIndicators[0]);
+    });
+  });
+});
