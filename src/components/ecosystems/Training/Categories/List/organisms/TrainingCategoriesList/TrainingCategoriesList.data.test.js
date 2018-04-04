9ee22dd1a05b18fc878a9c42f41b634edd6fff52
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2018-04-04 11:18:48 -0300

    fix(training-section-additional-params): Adding some params to training section queries

--- src/components/ecosystems/Training/Categories/List/organisms/TrainingCategoriesList/TrainingCategoriesList.data.test.js
+++ src/components/ecosystems/Training/Categories/List/organisms/TrainingCategoriesList/TrainingCategoriesList.data.test.js
@@ -9,14 +9,49 @@ describe('TrainingCategoriesQuery', () => {
   });
 
   it('should be the correct query options', () => {
-    const props = { user: { codigo: 1 } };
+    const user = {
+      codigo: 4064925,
+      cdCanalCaptacao: 8,
+      codigoCentro: 5800,
+      nomeCompleto: 'Fulano Ciclano',
+      estrutura: {
+        codigo: 7416,
+        codigoTipo: 5,
+        ciclo: [
+          {
+            numero: 201805,
+          },
+        ],
+        gerenciaMercado: {
+          codigo: 2,
+        },
+        regiaoEstrategica: {
+          codigo: 2,
+        },
+        gerenciaVenda: {
+          codigo: 23,
+        },
+        setor: {
+          codigo: 488,
+        },
+      },
+    };
+    const props = { user };
 
     const options = TrainingCategoriesQueryOptions.options(props);
 
     expect(options).toEqual({
       variables: {
-        sellerId: 1,
+        sellerId: props.user.codigo,
         limit: 5,
+        ciclo: props.user.estrutura.ciclo[0].numero,
+        setor: props.user.estrutura.setor.codigo,
+        gerenciaMercado: props.user.estrutura.gerenciaMercado.codigo,
+        grupo: props.user.estrutura.codigo,
+        papelDaConsultora: props.user.cdPapelAtivo,
+        canal: props.user.cdCanalCaptacao,
+        gerenciaDeVendas: props.user.estrutura.gerenciaVenda.codigo,
+        regiao: props.user.estrutura.regiaoEstrategica.codigo,
       },
       forceFetch: true,
       fetchPolicy: 'cache-and-network',
--- src/components/ecosystems/Training/data/CourseView.data.js
+++ src/components/ecosystems/Training/data/CourseView.data.js
@@ -1,8 +1,30 @@
 import gql from 'graphql-tag';
 
 export const CourseViewQuery = gql`
-  query CourseViewQuery($sellerId: Int!, $courseId: Int!) {
-    course(sellerId: $sellerId, courseId: $courseId) {
+  query CourseViewQuery(
+    $sellerId: Int!
+    $courseId: Int!
+    $ciclo: Int
+    $setor: Int
+    $gerenciaMercado: Int
+    $grupo: Int
+    $papelDaConsultora: Int
+    $canal: Int
+    $gerenciaDeVendas: Int
+    $regiao: Int
+  ) {
+    course(
+      sellerId: $sellerId
+      courseId: $courseId
+      ciclo: $ciclo
+      setor: $setor
+      gerenciaMercado: $gerenciaMercado
+      grupo: $grupo
+      papelDaConsultora: $papelDaConsultora
+      canal: $canal
+      gerenciaDeVendas: $gerenciaDeVendas
+      regiao: $regiao
+    ) {
       id
       courseContent {
         video
@@ -39,6 +61,17 @@ export const CourseViewQueryOptions = {
       variables: {
         sellerId: props.user.codigo,
         courseId: props.courseId,
+        ciclo:
+          props.user.estrutura.ciclo &&
+          props.user.estrutura.ciclo[0] &&
+          props.user.estrutura.ciclo[0].numero,
+        setor: props.user.estrutura.setor.codigo,
+        gerenciaMercado: props.user.estrutura.gerenciaMercado.codigo,
+        grupo: props.user.estrutura.codigo,
+        papelDaConsultora: props.user.cdPapelAtivo,
+        canal: props.user.cdCanalCaptacao,
+        gerenciaDeVendas: props.user.estrutura.gerenciaVenda.codigo,
+        regiao: props.user.estrutura.regiaoEstrategica.codigo,
       },
       forceFetch: true,
     };
