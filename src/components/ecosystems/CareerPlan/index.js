a4911859d2e588a322512bbda287e125f29765e1
Leonardo Cardoso <leonardocardosodealmeida@gmail.com.br>
2018-04-04 17:59:40 -0300

    feat(career-plan-indicator-integration): Create currentYear and pastYear param for GraphQL queries

--- src/components/ecosystems/CareerPlan/index.js
+++ src/components/ecosystems/CareerPlan/index.js
@@ -7,11 +7,20 @@ import { Wrapper } from './index.styles';
 
 export default class CareerPlanWrapper extends Component {
   currentCycle = parseInt(moment().format('YYYYMM'), 10);
+  currentYear = moment().year();
+  pastYear = moment()
+    .subtract(1, 'years')
+    .year();
 
   render() {
     return (
       <Wrapper>
-        <CareerPlan user={this.props.user} currentCycle={this.currentCycle} />
+        <CareerPlan
+          user={this.props.user}
+          currentCycle={this.currentCycle}
+          currentYear={this.currentYear}
+          pastYear={this.pastYear}
+        />
       </Wrapper>
     );
   }
