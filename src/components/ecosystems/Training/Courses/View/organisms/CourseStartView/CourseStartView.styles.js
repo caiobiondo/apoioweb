a2359b9fb02fbff05e589efa0f79fee5d258544b
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2018-04-11 16:53:50 -0300

    feat(training-course-html5-azure): Removing unneeded code

--- src/components/ecosystems/Training/Courses/View/organisms/CourseStartView/CourseStartView.styles.js
+++ src/components/ecosystems/Training/Courses/View/organisms/CourseStartView/CourseStartView.styles.js
@@ -221,25 +221,3 @@ export const IconWrapper = styled.div`
   }
 }
 `;
-
-export const Html5Wrapper = styled.div`
-  position: relative;
-  padding-bottom: 70%;
-  height: 0;
-  width: 100%;
-  overflow: hidden;
-
-  @media (max-width: ${Responsive.VIEWPORT.SMALL}px) {
-    padding-bottom: 100%;
-  }
-
-  iframe, object, embed {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    border-radius: 4px;
-  }
-}
-`;
