83dd012e646c69b43d150bb3fe12f16654bcf466
Edson Kenji Ninomiya <kenjinino07@gmail.com>
2018-05-02 12:44:54 -0300

    fix(webfv-appcn-access-token): removing unnecessary comment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
 export const GRAPHQL_URI = process.env.GRAPHQL_URI;
 
 export const ACCESS_TOKEN_LOCAL_STORAGE_KEY = 'accessTokenNaturaWeb';
-// export const ACCESS_TOKEN_LOCAL_STORAGE_KEY = 'sso';
 export const CNO_TOKEN_LOCAL_STORAGE_KEY = 'cnoTokenNaturaWeb';
 export const SEARCH_ZIP_URL = 'http://www.buscacep.correios.com.br/sistemas/buscacep/buscaCep.cfm';
 export const PERSON_ID_LOCAL_STORAGE_KEY = 'personIdNaturaWeb';
